add7f7e2aa20759eb5ed7a2130e6d2b67e6b9484
nshephe4 nshephe4@vols.utk.edu <nshephe4@vols.utk.edu>
2025-04-17 13:48:17 -0400

    updated test

--- odotets.py
+++ odotets.py
@@ -4,5 +4,5 @@ from transformers import pipeline
 messages = [
     {"role": "user", "content": "Who are you?"},
 ]
-pipe = pipeline("text-generation", model="meta-llama/Llama-3.1-70B-Instruct")
+pipe = pipeline("text-generation", model="meta-llama/Llama-4-Scout-17B-16E-Instruct")
 pipe(messages)
