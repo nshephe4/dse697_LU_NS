e8372e68b4a5e8b7abd3690aa3c95d465ef42b88
nshephe4 nshephe4@vols.utk.edu <nshephe4@vols.utk.edu>
2025-04-17 13:20:25 -0400

    updated test file

--- odotets.py
+++ odotets.py
@@ -1,1 +1,8 @@
-print("Github on odo is working")
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+messages = [
+    {"role": "user", "content": "Who are you?"},
+]
+pipe = pipeline("text-generation", model="meta-llama/Llama-3.1-405B-Instruct")
+pipe(messages)
